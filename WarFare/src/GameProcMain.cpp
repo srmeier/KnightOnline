62439fab7d047be82418a86c73f574a6d10af581
Stephen Meier <info@stephenmeier.net>
2016-01-24 12:24:49 -0500

    commiting once before added old Ebenezer server

--- WarFare/src/GameProcMain.cpp
+++ WarFare/src/GameProcMain.cpp
@@ -3933,16 +3933,27 @@ void CGameProcMain::MsgSend_UserInRequest(int iID) // User 정보가 없을 경우 요청
 
 void CGameProcMain::MsgSend_Warp() // 워프 - 존이동이 될수도 있다..
 {
+	// NOTE: !!! temp removing this for offline wrapping !!!
+	
 	__WarpInfo WI;
 	int iSel = m_pUIWarp->InfoGetCur(WI);
 	if(iSel < 0 || WI.szName.empty()) return;
 
+	/*
 	BYTE byBuff[8];
 	int iOffset = 0;
-
+	`
 	CAPISocket::MP_AddByte(byBuff, iOffset, N3_WARP_LIST);
 	CAPISocket::MP_AddByte(byBuff, iOffset, WI.iID); // 워프 아이디 보내기...
 	s_pSocket->Send(byBuff, iOffset);
+	*/
+
+	__Vector3 vec3;
+	vec3.x = 361.278503f;
+	vec3.y = 2.822370f;
+	vec3.z = 137.339859f;
+
+	InitZone(WI.iZone, vec3);
 }
 
 void CGameProcMain::MsgSend_ZoneChangeComplete() // 존 체인지 완료.. (맵 로딩 끝..)
@@ -7157,7 +7168,31 @@ bool CGameProcMain::OnMouseRBtnPress(POINT ptCur, POINT ptPrev)
 					}
 					else if(OBJECT_TYPE_WARP_POINT == pShape->m_iEventType)
 					{
-						this->MsgSend_ObjectEvent(pShape->m_iEventID, pShape->m_iNPC_ID); // 오브젝트 이벤트 보내기..
+						// NOTE: !!! currently working on offline warp portal !!!
+
+						//this->MsgSend_ObjectEvent(pShape->m_iEventID, pShape->m_iNPC_ID); // 오브젝트 이벤트 보내기..
+
+						m_pUIWarp->Reset();
+
+						for(int i = 0; i < 1; i++)
+						{
+							__WarpInfo WI;
+		
+							WI.iID = 0x0000;
+							WI.szName = "Moradon";
+							WI.szAgreement = "S:DLKFJS:LDKJF:LKJSDF";
+							WI.iZone = 0x0002;
+							WI.iMaxUser =0x0000;
+							WI.iGold = 0x00000000;
+							WI.vPos.x = 361.278503f;
+							WI.vPos.z = 2.822370f;
+							WI.vPos.y = 137.339859f;
+
+							m_pUIWarp->InfoAdd(WI);
+						}
+
+						m_pUIWarp->UpdateList();
+						m_pUIWarp->SetVisible(true);
 					}
 				}
 			}
