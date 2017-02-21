3a448c5b0b2ac4fce6b07f72676fff2383ec6a4d
Stephen Meier <info@stephenmeier.net>
2017-02-21 08:43:23 -0500

    Made slight change to icon verts so that item icons display in the center.

--- ItemEditor/ItemInfo.h
+++ ItemEditor/ItemInfo.h
@@ -6,6 +6,14 @@
 #include "STLMap.h"
 #include "N3TableBase.h"
 
+/*
+- goal is to be able to just plug one of these guys into the "Fl_Tabs" and have
+	all their information be displayed
+- this can house all the items and create a copy of the selected one which the
+	user can modify until they go to save their changes (just need to keep
+	track of the index/itorator)
+*/
+
 //-----------------------------------------------------------------------------
 class ItemInfo {
 private:
