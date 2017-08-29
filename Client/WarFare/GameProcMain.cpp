bd9d11cd6469d287bff3bfbc7a049b12c5b9b425
SavvyNik <nikolasvy@gmail.com>
2017-08-28 22:24:05 -0500

    Camera now only turns if window has mouse focus
    Added a bool to check if the game window has mouse focus from an SDL event.  I also put conditional around the rotation of the camera in the case that a user moves to the edge of the screen with their mouse. Now the character will no longer rotate camera if they leave the game window with their mouse.

--- Client/WarFare/GameProcMain.cpp
+++ Client/WarFare/GameProcMain.cpp
@@ -1117,20 +1117,23 @@ void CGameProcMain::ProcessLocalInput(uint32_t dwMouseFlags)
 	//	s_pLocalInput->MouseSetPos(ptPrev_RB.x+x, ptPrev_RB.y+y);
 	//}
 
+
 	// Moves camera when mouse is on the borders of the screen. For both X & Y
-	// SavvyNik - Why was this commented out?
+	// Now checking to make sure game window has focus of mouse
 	if (!(dwMouseFlags & MOUSE_RBDOWN)) {
-		float fRotY = 0, fRotX = 0;
-		if (0 == ptCur.x) fRotY = -2.0f;
-		else if ((CN3Base::s_CameraData.vp.Width - 1) == ptCur.x) fRotY = 2.0f;
-		if (0 == ptCur.y) fRotX = -1.0f;
-		else if ((CN3Base::s_CameraData.vp.Height - 1) == ptCur.y) fRotX = 1.0f;
-		if (fRotY)
-		{
-			if (VP_THIRD_PERSON == s_pEng->ViewPoint()) s_pEng->CameraYawAdd(fRotY);
-			else s_pPlayer->RotAdd(fRotY);
+		if (CGameProcedure::s_bWindowHasMouseFocus) {
+			float fRotY = 0, fRotX = 0;
+			if (0 == ptCur.x) fRotY = -2.0f;
+			else if ((CN3Base::s_CameraData.vp.Width - 1) == ptCur.x) fRotY = 2.0f;
+			if (0 == ptCur.y) fRotX = -1.0f;
+			else if ((CN3Base::s_CameraData.vp.Height - 1) == ptCur.y) fRotX = 1.0f;
+			if (fRotY)
+			{
+				if (VP_THIRD_PERSON == s_pEng->ViewPoint()) s_pEng->CameraYawAdd(fRotY);
+				else s_pPlayer->RotAdd(fRotY);
+			}
+			if (fRotX && VP_THIRD_PERSON != s_pEng->ViewPoint()) s_pEng->CameraPitchAdd(fRotX);
 		}
-		if (fRotX && VP_THIRD_PERSON != s_pEng->ViewPoint()) s_pEng->CameraPitchAdd(fRotX);
 	}
 
 
