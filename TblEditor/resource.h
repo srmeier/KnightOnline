99b3705a223b33cdfe4a0ad4bfb98a05943619e1
koraykarakus <koraykarakus@yahoo.com>
2025-05-01 19:07:09 +0300

    Tbl editor for OpenKO

---
+++ TblEditor/resource.h
@@ -0,0 +1,33 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by TblEditor.rc
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_TBLEDITOR_DIALOG            102
+#define IDS_ERROR_WRONG_FILE_FORMAT     102
+#define IDS_ERROR_FILE_NOT_OPENED       103
+#define IDS_ERROR_NO_DATA_FOUND         104
+#define IDS_SAVE_SUCCESS                105
+#define IDS_SAVE_FAILED                 106
+#define IDR_MAINFRAME                   128
+#define IDR_MENU1                       130
+#define IDC_COMBO1                      1000
+#define IDC_LIST1                       1001
+#define IDC_EDIT1                       1002
+#define IDC_BUTTON1                     1003
+#define IDC_BUTTON2                     1004
+#define IDC_BUTTON3                     1005
+#define ID_FILE_NEW32771                32771
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_COMMAND_VALUE         32772
+#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
