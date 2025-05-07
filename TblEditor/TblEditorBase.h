f5d9b762ae3a278375a20f195248b42b6c62e7c0
twostars <i.am.twostars@gmail.com>
2025-05-07 19:57:37 +1000

    Expose DATA_TYPE from N3TableBase.h

--- TblEditor/TblEditorBase.h
+++ TblEditor/TblEditorBase.h
@@ -4,19 +4,9 @@
 #include <map>
 #include <vector>
 
-enum DATA_TYPE : int32_t
-{
-	DT_NONE,
-	DT_CHAR,
-	DT_BYTE,
-	DT_SHORT,
-	DT_WORD,
-	DT_INT,
-	DT_DWORD,
-	DT_STRING,
-	DT_FLOAT,
-	DT_DOUBLE
-};
+#include <N3Base/N3TableBase.h>
+
+using DATA_TYPE = TBL_DATA_TYPE;
 
 class CTblEditorBase
 {
